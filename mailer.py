c800fe677f82d547f204f5c124644ef78516d0df
llwang8 <wangxbl56@gmail.com>
2016-07-07 23:13:06 -0400

    initial commit

---
+++ mailer.py
@@ -0,0 +1,32 @@
+def get_emails():
+    emails = []
+
+    try:
+        email_file = open('emails.txt', 'r')
+
+        for line in email_file:
+            (email, name) = line.split(',')
+            emails[email] = name.strip()
+
+    except FileNotFoundError as err:
+        print(err)
+
+    return emails
+
+def get_schedule():
+    try:
+        schedule_file = open('schedule.txt', 'r')
+
+        schedule = schedule_file.read()
+
+    return schedule
+
+def main():
+    emails = get_emails()
+    print(emails)
+
+    schedule = get_schedule()
+    print (schedule)
+
+
+
